853927768158fb14a39a4ba4416fa0c5882a853c
Simon Rozet <simon@rozet.name>
2009-10-05 14:14:59 +0200

    No need for SCM#class_for

--- lib/bob/scm.rb
+++ lib/bob/scm.rb
@@ -9,17 +9,7 @@ module Bob
 
     # Factory to return appropriate SCM instances (according to repository kind)
     def self.new(scm, uri, branch)
-      class_for(scm).new(uri, branch)
+      const_get(scm.to_s.capitalize).new(uri, branch)
     end
-
-    # A copy of Inflector.camelize, from ActiveSupport. It will convert
-    # string to UpperCamelCase.
-    def self.class_for(string)
-      class_name = string.to_s.
-        gsub(/\/(.?)/) { "::#{$1.upcase}" }.
-        gsub(/(?:^|_)(.)/) { $1.upcase }
-      const_get(class_name)
-    end
-    private_class_method :class_for
   end
 end
